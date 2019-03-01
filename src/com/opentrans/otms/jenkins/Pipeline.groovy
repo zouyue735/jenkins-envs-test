57e01eb15408d1319596b04a3a1c5c7b3619f2f2
Ralf Zou <ralf.zou@otms.cn>
2019-03-01 17:29:05 +0100

    Test

--- src/com/opentrans/otms/jenkins/Pipeline.groovy
+++ src/com/opentrans/otms/jenkins/Pipeline.groovy
@@ -3,7 +3,9 @@ package com.opentrans.otms.jenkins
 public class Pipeline {
 
     static {
-        System.out.println 'Loaded Pipeline'
+        script {
+            echo 'Loaded Pipeline'
+        }
     }
 
     public static void print() {
