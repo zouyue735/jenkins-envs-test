3fdb4e2f3ca86294068e94c64cee106397e7a7fb
Ralf Zou <ralf.zou@otms.cn>
2019-03-01 21:26:58 +0100

    Test

--- src/com/opentrans/otms/jenkins/Modules.groovy
+++ src/com/opentrans/otms/jenkins/Modules.groovy
@@ -3,16 +3,20 @@ package com.opentrans.otms.jenkins
 import java.util.Map
 
 public class Modules {
-
-    private static Modules instance = new Modules();
+    private static Modules instance
+        
+    private static Modules getInstance() { 
+        if (!instance) instance = new Modules(args)
+        return instance
+    }
 
     private Map modules = [:];
 
     public static void register(String name, String module) {
-        instance.modules.put(name, module);
+        getInstance().modules.put(name, module);
     }
 
     public static Map getModules() {
-        return instance.modules;
+        return getInstance().modules;
     }
 }
