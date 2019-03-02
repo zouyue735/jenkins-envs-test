5b993eb939cf5f1c4f04e46d1d1ff6ff6fdb7b18
Ralf Zou <ralf.zou@otms.cn>
2019-03-02 16:58:40 +0100

    Test

--- src/model/Environment.groovy
+++ src/model/Environment.groovy
@@ -4,9 +4,9 @@ class Environment implements Named, java.io.Serializable {
 
     private String name
 
-    private Set<Class<? extends Module>> modules = []
-    private Map<String, Component> components = [:]
-    private Map<String, Configuration> configurations = [:]
+    //private Set<Class<? extends Module>> modules = []
+    //private Map<String, Component> components = [:]
+    //private Map<String, Configuration> configurations = [:]
 
     Environment(String name) {
         this.name = name
@@ -17,16 +17,16 @@ class Environment implements Named, java.io.Serializable {
         return this.name
     }
 
-    def registerModules(Collection<Class<? extends Module>> modules) {
-        this.modules.addAll(modules)
-    }
-
-    def registerComponents(Collection<Component> components) {
-        
-    }
-
-    //@Override
-    //String toString() {
-    //    return "modules: " + this.modules + ", components: " + this.components + ", configurations: " + this.configurations
+    //def registerModules(Collection<Class<? extends Module>> modules) {
+    //    this.modules.addAll(modules)
     //}
+
+    //def registerComponents(Collection<Component> components) {
+        
+    //}
+
+    @Override
+    String toString() {
+        return "modules: " + this.modules + ", components: " + this.components + ", configurations: " + this.configurations
+    }
 }
