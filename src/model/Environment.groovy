d86c7b8c485f0ff7db09dd026dccc9a7f4e2a1ca
Ralf Zou <ralf.zou@otms.cn>
2019-03-02 17:09:45 +0100

    Test

--- src/model/Environment.groovy
+++ src/model/Environment.groovy
@@ -1,6 +1,6 @@
 package model
 
-class Environment implements Named, java.io.Serializable {
+class Environment implements java.io.Serializable {
 
     //private String name
 
@@ -12,7 +12,7 @@ class Environment implements Named, java.io.Serializable {
         //this.name = name
     //}
 
-    @Override
+    // @Override
     String name() {
         //return this.name
         return ''
